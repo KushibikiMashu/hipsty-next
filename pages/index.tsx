39f55706e7d323b63c8c3e273b1d5df1abdc8c70
KushibikiMashu <kushibiki3413@gmail.com>
2020-08-11 04:40:27 +0900

    データ取得をサーバーで行う

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import VideoList from '../src/components/pages/VideoList'
 import VideoService from 'src/infra/VideoService'
 import { Videos } from '../src/types/Video'
+import { GetStaticProps } from 'next'
 
 type Props = {
   videos: Videos
@@ -9,14 +10,12 @@ type Props = {
 
 const Component: React.FC<Props> = (props) => <VideoList videos={props.videos} />
 
-type ContainerProps = unknown
-
-const Container: React.FC<ContainerProps> = () => {
+export const getStaticProps: GetStaticProps<Props> = async () => {
   const videos = VideoService.getAllVideo()
 
-  return <Component videos={videos} />
+  return { props: { videos } }
 }
 
-Container.displayName = 'IndexPage'
+Component.displayName = 'IndexPage'
 
-export default Container
+export default Component
