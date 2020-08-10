fca6bbadcc443c45b323ba47a092fa485c00e22f
KushibikiMashu <kushibiki3413@gmail.com>
2020-08-11 04:41:05 +0900

    コメント削除

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import Head from 'next/head'
 import Layout from '../src/components/Layout'
 
 const MyApp = ({ Component, pageProps }) => (
-  // TODO video/:hash はレイアウトを変える？
   <>
     <Head>
       <title>HIPSTY | 日本語ラップ好きのための動画サイト</title>
