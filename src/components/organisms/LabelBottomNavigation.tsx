e20c0b8280b10f36b4604de4b566f3aa94675556
KushibikiMashu <kushibiki3413@gmail.com>
2020-08-11 15:19:32 +0900

    bottom navigationを改修

--- src/components/organisms/LabelBottomNavigation.tsx
+++ src/components/organisms/LabelBottomNavigation.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import BottomNavigation from '@material-ui/core/BottomNavigation'
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction'
 import FiberNewIcon from '@material-ui/icons/FiberNew'
 import MusicVideoIcon from '@material-ui/icons/MusicVideo'
@@ -13,39 +12,40 @@ import { createStyles, WithStyles } from '@material-ui/styles'
 type Props = WithStyles<typeof styles>
 
 const Component: React.FC<Props> = (props) => (
-  <BottomNavigation className={props.classes.root}>
+  <div className={props.classes.root}>
     <Link href="/">
       <a>
         <BottomNavigationAction label="New" icon={<FiberNewIcon />} />
       </a>
     </Link>
-    <Link href="/videos/music-videos">
+    <Link href="/videos/[slug]" as="/videos/music-videos">
       <a>
         <BottomNavigationAction label="MV" icon={<MusicVideoIcon />} />
       </a>
     </Link>
-    <Link href="/videos/battles">
+    <Link href="/videos/[slug]" as="/videos/battles">
       <a>
         <BottomNavigationAction label="Battle" icon={<NewReleasesIcon />} />
       </a>
     </Link>
-    <Link href="/videos/interviews">
+    <Link href="/videos/[slug]" as="/videos/interviews">
       <a>
         <BottomNavigationAction label="Interview" icon={<PersonIcon />} />
       </a>
     </Link>
-    <Link href="/videos/others">
+    <Link href="/videos/[slug]" as="/videos/others">
       <a>
         <BottomNavigationAction label="Others" icon={<LiveTvIcon />} />
       </a>
     </Link>
-  </BottomNavigation>
+  </div>
 )
 
 const styles = createStyles({
   root: {
     width: '100%',
     textAlign: 'center',
+    backgroundColor: '#FFF',
   },
 })
 
