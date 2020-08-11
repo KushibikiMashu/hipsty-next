260de844f7b813b9db29681a6a3e243bd8f32c4d
KushibikiMashu <kushibiki3413@gmail.com>
2020-08-11 14:26:35 +0900

    ジャンルごとのページを追加

---
+++ pages/videos/[slug]/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { genreToSlug, slugToGenre } from 'src/presenters/VideoPresenter'
+import GenreService from '../../../src/infra/GenreService'
+import VideoService from '../../../src/infra/VideoService'
+import { GetStaticPaths, InferGetStaticPropsType } from 'next'
+import VideoList from '../../../src/components/pages/VideoList'
+
+type Props = InferGetStaticPropsType<typeof getStaticProps>
+
+const Component: React.FC<Props> = (props) => <VideoList videos={props.videos} />
+
+export const getStaticProps = async (props) => {
+  const genre = slugToGenre(props.params.slug)
+  const videos = VideoService.getVideosByGenre(genre)
+
+  return { props: { videos } }
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const slugs = GenreService.genresToLinks().map((genre) => genreToSlug(genre))
+  const params = slugs.map((slug) => ({ params: { slug } }))
+
+  return { paths: params, fallback: false }
+}
+
+Component.displayName = 'CategoryPage'
+
+export default Component
