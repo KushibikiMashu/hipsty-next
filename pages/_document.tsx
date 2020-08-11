71567766ddee136d19e58490df07756fb46232cd
KushibikiMashu <kushibiki3413@gmail.com>
2020-08-11 15:45:11 +0900

    微調整

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,14 +13,14 @@ class MyDocument extends Document {
       <Html>
         <Head lang="ja">
           <meta name="Content-Type" content="text/html; charset=UTF-8" />
-          <link rel="canonical" href="https://hipsty.net/" />
+          <link rel="canonical" href="https://hipsty.vercel.app/" />
           <meta property="og:site_name" content="HIPSTY | 日本語ラップ好きのための動画サイト" />
           <meta property="og:title" content="HIPSTY" />
           <meta
             property="og:description"
             content="MCバトルも新曲もラッパーのインタビューも！最新の日本語ラップ動画が見つかるサイト"
           />
-          <meta property="og:url" content="https://hipsty.net/" />
+          <meta property="og:url" content="https://hipsty.vercel.app/" />
           <meta property="og:image" content="https://s3-ap-northeast-1.amazonaws.com/hipsty/hipsty.jpg" />
           <meta property="og:type" content="website" />
           <meta name="twitter:site" content="@Panda_Program" />
@@ -31,10 +31,9 @@ class MyDocument extends Document {
           />
           <meta name="twitter:image" content="https://s3-ap-northeast-1.amazonaws.com/hipsty/hipsty.jpg" />
           <meta name="twitter:card" content="summary_large_image" />
-          <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet" />
         </Head>
         <CssBaseline />
-        <body>
+        <body style={{ margin: 0 }}>
           <Main />
           <NextScript />
         </body>
